0db32a4d42b2b188a9f557439e565ccdeed717f7
reygenc <reygenc@yahoo.com>
2023-11-21 22:36:13 -0800

    removed context

--- src/components/context.js
+++
@@ -1,5 +0,0 @@
-// const Link = ReactRouterDOM.Link;
-import { createContext } from "react";
-export const UserContext = createContext(null);
-
-// TODO: remove all context and this file to simplify app.
